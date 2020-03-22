54c5e99a17babf8e93dcf93e82c9771c857f51c1
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2020-03-22 22:28:28 +0530

    Initial Commit

---
+++ worldSim macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  worldSim macOS
+//
+//  Created by Harjot Singh on 22/03/20.
+//  Copyright © 2020 Harjot Singh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
